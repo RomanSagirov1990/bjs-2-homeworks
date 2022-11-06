9aa321c6a292cc03eb0aeba1c4d2c857c092e495
Roman Sagirov <Naytler12@yandex.ru>
2022-11-06 14:08:47 +0300

    add Array

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,14 +1,14 @@
 function compareArrays(arr1, arr2) {
   let result;
-
+  every = (el,ind,arr) => el == arr2[ind] && arr.length == arr2.length;       
+  result = arr1.every(every)
   // Ваш код
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
+  let resultArr = arr.filter(el => el >= 0 && el % 3 == 0).map( el => el * 10)
   // Ваш код
 
   return resultArr; // array
