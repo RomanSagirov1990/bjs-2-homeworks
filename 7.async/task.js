52ca6b6540a32ab442242743c768beba780248e7
Roman Sagirov <Naytler12@yandex.ru>
2022-11-27 17:13:43 +0300

    add async 2

--- 7.async/task.js
+++ 7.async/task.js
@@ -3,14 +3,15 @@ class AlarmClock {
         this.alarmCollection = [];
         this.timerId = null;
     }
-    addClock(time, Callback, id) {
-        if (!id) throw new Error('Error: невозможно индетифицировать будильник. Параметр id не передан')
-
+    addClock(time, сallback, id) {
+        if (!id) {
+            throw new Error('Error: невозможно индетифицировать будильник. Параметр id не передан')
+        }
         if (this.alarmCollection.some(el => el.id === id)) {
             console.error(`Будильник с таким id ${id} уже существует`);
             return;
         };
-        this.alarmCollection.push({ id, time, Callback })
+        this.alarmCollection.push({ id, time, сallback })
     }
     removeClock(id) {
         if (this.alarmCollection.some(el => el.id === id)) {
@@ -18,33 +19,41 @@ class AlarmClock {
             return true
         } else return false
     }
-    getCurrentFormattedTime(){
+    getCurrentFormattedTime() {
         let date = new Date();
         let hours = date.getHours();
-        if (hours < 10) hours = '0' + hours;
+        if (hours < 10) {
+            hours = '0' + hours;
+        }
         let mins = date.getMinutes();
-        if (mins < 10) mins = '0' + mins;
+        if (mins < 10) {
+            mins = '0' + mins;
+        }
         let output = 'h:m'.replace('h', hours).replace('m', mins)
         return output;
     }
     start() {
         let checkAlarm = checkClock.bind(this);
-        function checkClock(objAlarmСlock) {
-            if (objAlarmСlock.time === this.getCurrentFormattedTime()) objAlarmСlock.func();
+        function checkClock(callback) {
+            if (callback.time === this.getCurrentFormattedTime()) {
+                return checkAlarm;
+            }
         }
-        if (!this.timerId) this.timerId = setInterval(() => this.alarmCollection.forEach(el => checkAlarm(el)), 1000);
+        if (!this.timerId) {
+            this.timerId = setInterval(() => this.alarmCollection.forEach(el => checkAlarm(el)), 1000);
+        }
     }
-   stop() {
-    if (this.timerId) {
-        clearInterval(this.timerId);
-        this.timerId = null;
+    stop() {
+        if (this.timerId) {
+            clearInterval(this.timerId);
+            this.timerId = null;
+        }
     }
-   }
-   printAlarms() {
-    this.alarmCollection.forEach(el => console.log(`Будильник №${el.id} заведен на ${el.time}`));
-   }
-   clearAlarms() {
-    this.stop();
-    this.alarmCollection = [];
-   }
+    printAlarms() {
+        this.alarmCollection.forEach(el => console.log(`Будильник №${el.id} заведен на ${el.time}`));
+    }
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    }
 }
